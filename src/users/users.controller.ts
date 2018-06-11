befdc49152bdd8f879c93f107d59ac70c74f818f
danielflorez111 <danielflorez111@hotmail.com>
2018-06-11 12:40:34 -0500

    First commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private readonly usersService: UsersService) {}
+
+    // @Post()
+    // create(@Body() createUserDto) {
+    //     return 'This action adds a new user';
+    // }
+
+    @Post()
+    async create(@Body() createUserDto: CreateUserDto) {
+        return await this.usersService.create(createUserDto);
+    }
+
+    @Get()
+    async findAll() {
+        return await this.usersService.findAll();
+        // return 'This action returns all users';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id) {
+        return `This action returns a #${id} user`;
+    }
+
+    @Put(':id')
+    update(@Param('id') id, @Body() updateCatDto) {
+        return `This action updates a #${id} user`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id) {
+        return `This action removes a #${id} user`;
+    }
+
+}
